c1128c03a32b23478c0c902c744c71405eda08b9
Nick404 <nibirimtiaz1@gmail.com>
2026-03-07 15:28:25 +0600

    feat: Implement bubble sort algorithm for both ascending and descending order.

---
+++ Bubble Sort.py
@@ -0,0 +1,15 @@
+list = [9,2,7,4,1]
+n = len(list)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if list[j] > list[j+1]:
+            list[j], list[j+1] = list[j+1], list[j]
+print(list)
+
+numbers = [5,1,8,3]
+m= len(numbers)
+for i in range(m):
+    for j in range (0, m-i-1):
+        if numbers[j] < numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+print(numbers)
